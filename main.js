39a41a22a8373680a0cd27462ab6aa49acaceb45
Little Big Owl <littlebigowl@outlook.com>
2017-10-20 09:08:49 +0200

    change JS to es5 standard

--- main.js
+++ main.js
@@ -1,43 +1,42 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     console.log("DOM fully loaded and parsed");
-    let zapasyMain = document.getElementById("zapasyMain");
-    let hraciMain = document.getElementById("hraciMain");
+    var zapasyMain = document.getElementById("zapasyMain");
+    var hraciMain = document.getElementById("hraciMain");
 
-    let containerZapasy = document.getElementById("containerZapasy");
-    let containerHraci = document.getElementById("containerHraci");
+    var containerZapasy = document.getElementById("containerZapasy");
+    var containerHraci = document.getElementById("containerHraci");
 
     // TRUE = Zapasy, False = Hraci
-    let zapasyHraci = true;
+    var zapasyHraci = true;
 
     switchNav();
 
-    hraciMain.addEventListener("click",function(){
-        if(zapasyHraci){
+    hraciMain.addEventListener("click", function () {
+        if (zapasyHraci) {
             zapasyHraci = false;
             switchNav();
         }
     });
 
-    zapasyMain.addEventListener("click",function(){
-        if(!zapasyHraci){
+    zapasyMain.addEventListener("click", function () {
+        if (!zapasyHraci) {
             zapasyHraci = true;
             switchNav();
         }
     });
 
     // Switching between "Zapasy" and "Hraci"
-    function switchNav(){
-        if(zapasyHraci){
+    function switchNav() {
+        if (zapasyHraci) {
             zapasyMain.className = "nav nav-active";
             hraciMain.className = "nav";
             containerZapasy.style.display = "block";
             containerHraci.style.display = "none";
-        }else{
+        } else {
             zapasyMain.className = "nav";
             hraciMain.className = "nav nav-active";
             containerZapasy.style.display = "none";
             containerHraci.style.display = "block";
         }
     }
-
-  });
+});
